5aa69c828f9daf29fdb6a192facd217b6a5dfeec
Vlad Kochmar <vladkochmar17@gmail.com>
2026-05-05 12:49:00 +0200

    add tasks list and create task field

--- src/app/dashboard-page/dashboard-page.ts
+++ src/app/dashboard-page/dashboard-page.ts
@@ -1,30 +1,65 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { AuthService } from '@auth0/auth0-angular';
-import { Sidebar } from './components/sidebar/sidebar';
-import { TaskItem } from './components/task-item/task-item';
+import { Component } from '@angular/core';
+
 import { ITaskResponse } from '../core/api/todo-api';
+
+import { Sidebar } from './components/sidebar/sidebar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+import { TaskItem } from './components/task-item/task-item';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'todo-dashboard-page',
-  imports: [CommonModule, MatButtonModule, MatIconModule, Sidebar, TaskItem],
+  imports: [FormsModule, MatButtonModule, MatIconModule, Sidebar, TaskItem, MatInputModule],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
 export class DashboardPage {
-  protected auth = inject(AuthService);
-  protected task: ITaskResponse = {
-    id: '1',
-    title: 'Develope todo app',
-    isCompleted: false,
-    description: 'Some description of a task for better understanding.',
-    dueDate: '2026-05-5 16:05',
-    categoryName: 'Work',
-  };
+  protected tasks: ITaskResponse[] = [
+    {
+      id: '1',
+      title: 'Develope todo app',
+      isCompleted: false,
+      description: 'Some description of a task for better understanding.',
+      dueDate: '2026-05-5 16:05',
+      categoryName: 'Work',
+      categoryId: '2',
+    },
+    {
+      id: '2',
+      title: 'Learn C#',
+      isCompleted: true,
+      description: null,
+      dueDate: null,
+      categoryName: 'Work',
+      categoryId: '2',
+    },
+    {
+      id: '3',
+      title: 'Swim',
+      isCompleted: false,
+      description: null,
+      dueDate: null,
+      categoryName: null,
+      categoryId: null,
+    },
+  ];
 
-  logout() {
-    this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
+  protected newTaskTitle = '';
+
+  createTask() {
+    const title = this.newTaskTitle.trim();
+
+    if (!title) return;
+
+    console.log('create task:', title);
+
+    this.newTaskTitle = '';
+  }
+
+  clearInput() {
+    this.newTaskTitle = '';
   }
 }
